cd26904101c0256a49c703195e9407dad49f95b4
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-14 15:22:14 +0100

    Create User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/delete'
   root to: 'pages#home'
   resources :buildings, only: [:index, :new, :create, :show] do
     resources :flats
