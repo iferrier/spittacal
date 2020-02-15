d7b51c8108c735fb1293e84f3df5d47d8462f61d
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-15 23:52:36 +0100

    Add form for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_building
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.building = @building
+    @flat = Flat.find(user_params[:flat_id].to_i)
+    @user.flat = @flat
+    if @user.save
+      redirect_to building_flat_path(@building, @flat)
+    else
+      render :new
+    end
   end
 
   def index
@@ -24,5 +36,9 @@ class UsersController < ApplicationController
 
   def set_building
     @building = Building.find(params[:building_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :flat_id)
   end
 end
