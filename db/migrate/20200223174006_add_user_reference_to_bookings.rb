c37c78456365e072afbe1bbd8a5f2e4689984112
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-23 18:40:40 +0100

    Add user ref to bookings

---
+++ db/migrate/20200223174006_add_user_reference_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddUserReferenceToBookings < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :bookings, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_173905) do
+ActiveRecord::Schema.define(version: 2020_02_23_174006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_02_23_173905) do
     t.string "occasion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "buildings", force: :cascade do |t|
@@ -72,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_02_23_173905) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "buildings"
   add_foreign_key "resources", "buildings"
   add_foreign_key "templates", "users"
