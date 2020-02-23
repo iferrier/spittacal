1e7b0e3f4943acba2148676999647a3319ef5fcd
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-23 18:27:38 +0100

    Update User Model with active record relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :flat
+  belongs_to :building
+  has_many :templates, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
