f7dc0a2e67a60e6742dad0f6a6257d80fd5ee2c3
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-23 18:38:28 +0100

    Add flat ref to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_173625) do
+ActiveRecord::Schema.define(version: 2020_02_23_173737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,10 +64,13 @@ ActiveRecord::Schema.define(version: 2020_02_23_173625) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "flat_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["flat_id"], name: "index_users_on_flat_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "flats", "buildings"
   add_foreign_key "resources", "buildings"
+  add_foreign_key "users", "flats"
 end
