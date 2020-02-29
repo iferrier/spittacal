c7ffe9619a0efe75a184ed1f1655ca0606ff6992
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-02-29 18:22:04 +0100

    Create profiles controller and profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
+  get 'profile', to: 'profiles#profile'
   resources :buildings, only: [:index, :new, :create, :show] do
     resources :flats
     resources :users
