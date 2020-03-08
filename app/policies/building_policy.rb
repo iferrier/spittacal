0f0cd3c1f2aec30a9ee50d62da718de22874e7e5
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-08 23:38:47 +0100

    Add policies for building create/update/destroy

--- app/policies/building_policy.rb
+++ app/policies/building_policy.rb
@@ -5,13 +5,21 @@ class BuildingPolicy < ApplicationPolicy
     end
   end
 
+  def create
+    user_is_owner_or_admin
+  end
+
+  def update
+    user_is_owner_or_admin
+  end
+
+  def destroy
+    user_is_owner_or_admin
+  end
+
   private
 
   def user_is_owner_or_admin?
     record.user == user || user.admin
   end
-
-  # def user_is_owner_or_admin?
-  #   record.user == user || user.admin
-  # end
 end
