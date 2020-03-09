d74405d5805fd965097a1a32f28e38bc915682bc
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-10 00:17:19 +0100

    Add flat policy

--- app/policies/flat_policy.rb
+++ app/policies/flat_policy.rb
@@ -4,4 +4,26 @@ class FlatPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    user_is_allowed?
+  end
+
+  def show?
+    user_is_allowed?
+  end
+
+  def update?
+    user_is_allowed?
+  end
+
+  def destroy?
+    user_is_allowed?
+  end
+
+  private
+
+  def user_is_allowed?
+    user.housecaretaker || user.admin
+  end
 end
