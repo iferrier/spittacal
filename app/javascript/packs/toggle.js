3ff116113b8e48a414e519938eaf801fbc1065fc
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-14 23:18:36 +0100

    Return toggle with Javascript, for all the list elements

--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -1,23 +1,25 @@
-const list = document.querySelector('.lists');
-const nodeNames = document.querySelectorAll('.name');
-const names = Array.from(nodeNames);
-const description = document.querySelector('.description')
+const names = document.querySelectorAll('.name');
+const description = document.querySelector('.description');
 
-console.log(list);
 
-const toggleContent = () => {
-  list.addEventListener('click', event => {
-    const clickedItem = names.indexOf(event.target);
-    console.log(names[clickedItem]);
+const toggleContent = () => { names.forEach(name => {
+    name.addEventListener('click', event => {
+      console.log(event);
+      description.classList.toggle('is-visible');
+    })
   })
 };
 
-// const toggleContent = () => { names.forEach(name => {
-//     name.addEventListener('click', event => {
-//       console.log(event);
-//       description.classList.toggle('is-visible');
-//     })
+export { toggleContent };
+
+// Other way, not successfull
+
+// const list = document.querySelector('.lists');
+// const nodeNames = document.querySelectorAll('.name');
+// const names = Array.from(nodeNames);
+// const toggleContent = () => {
+//   list.addEventListener('click', event => {
+//     const clickedItem = names.indexOf(event.target);
+//     console.log(names[clickedItem]);
 //   })
 // };
-
-export { toggleContent };
