5fcc427b513c9df5c5143bb686c092ee59fdfcb5
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-14 23:30:33 +0100

    Add styling to list

--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -4,7 +4,6 @@ const description = document.querySelector('.description');
 
 const toggleContent = () => { names.forEach(name => {
     name.addEventListener('click', event => {
-      console.log(event);
       description.classList.toggle('is-visible');
     })
   })
