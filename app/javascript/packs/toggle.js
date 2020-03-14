0af27818a31c20c055988dbfbc0894333d3693b8
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-14 22:08:43 +0100

    Test with bootstrap accordeon aborted

--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -1,14 +1,23 @@
-// Toggle content
-
-const names = document.querySelectorAll('.name');
+const list = document.querySelector('.lists');
+const nodeNames = document.querySelectorAll('.name');
+const names = Array.from(nodeNames);
 const description = document.querySelector('.description')
 
-const toggleContent = () => { names.forEach(name => {
-    name.addEventListener('click', event => {
-      console.log(event);
-      description.classList.toggle('is-visible');
-    })
+console.log(list);
+
+const toggleContent = () => {
+  list.addEventListener('click', event => {
+    const clickedItem = names.indexOf(event.target);
+    console.log(names[clickedItem]);
   })
 };
+
+// const toggleContent = () => { names.forEach(name => {
+//     name.addEventListener('click', event => {
+//       console.log(event);
+//       description.classList.toggle('is-visible');
+//     })
+//   })
+// };
 
 export { toggleContent };
