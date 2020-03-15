c2545fd48446291e21ac5f8db05203f688c649c5
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-15 22:53:40 +0100

    Generate invitation controller and associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'users/invitations' }
   get 'profile', to: 'profiles#user_profile'
   get 'a-profile', to: 'profiles#admin_profile'
   get 'hct-profile', to: 'profiles#housecaretaker_profile'
