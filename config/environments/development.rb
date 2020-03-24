cf761ba1976cf8a2f7dbd06066b7df4c0277b2eb
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-24 23:09:42 +0100

    Add Invitation controller and change config for sending mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.delivery_method = :sendmail
 
   config.action_mailer.perform_caching = false
 
