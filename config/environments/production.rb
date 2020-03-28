7b9c008fea1739081d8adaab7aed081e2e4ba575
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-28 18:33:08 +0100

    Update config mailing in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "spittacal_#{Rails.env}"
 
+  # Config Mailing
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
