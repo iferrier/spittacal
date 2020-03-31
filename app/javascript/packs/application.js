c84f2d56c20f631424cedc8578263e3a999b4dfe
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-03-31 15:25:55 +0200

    Add js file logout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import { toggleContent } from './toggle';
+import { redirectLogout } from './logout';
 
 // Toggle content
 toggleContent();
 
+// Logout Redirect
+redirectLogout();
